a0a7cce25350a02361d000d1c11fadd0bbb8792d
FlorianxK <Florian.Koehler@jokonet.de>
2025-11-04 14:18:50 +0100

    day21 finished

---
+++ Day21/day21.py
@@ -0,0 +1,189 @@
+from collections import deque
+from functools import cache
+from itertools import product
+from typing import *
+
+def dayTwentyone():
+
+    #read
+    with open("Day21/21_2.txt", 'r') as file:
+        lines = list(map(str.strip, file.readlines()))
+
+    def find_best_x_to_y(keypad, x, y):
+        if x == y:
+            return ["A"]
+        # row, column, path
+        queue = deque([(*x, "")])
+        best_length = 10
+        optimal_paths = []
+        while queue:
+            r, c, path = queue.popleft()
+            for nr, nc, move in [
+                (r + 1, c, "v"),
+                (r - 1, c, "^"),
+                (r, c + 1, ">"),
+                (r, c - 1, "<"),
+            ]:
+                if (nr, nc) not in keypad:
+                    continue
+                if (nr, nc) == y and len(path) + 1 > best_length:
+                    break
+                if (nr, nc) == y:
+                    optimal_paths.append(path + move + "A")
+                    best_length = min(len(path) + 2, best_length)
+                else:
+                    queue.append((nr, nc, path + move))
+            else:
+                continue
+            break
+        return optimal_paths
+
+
+    def keys_to_paths(keypad_paths, code):
+        paths = [keypad_paths[(b1, b2)] for b1, b2 in zip("A" + code, code)]
+        return ["".join(segment) for segment in product(*paths)]
+
+    @cache
+    def get_length(code, depth):
+        if depth == 0:
+            return len(code)
+        length = 0
+        for p1, p2 in zip("A" + code, code):
+            length += min(get_length(seq, depth - 1) for seq in dir_pad_paths[(p1, p2)])
+        return length
+
+    num_pad = {
+        (0, 0): "7",
+        (0, 1): "8",
+        (0, 2): "9",
+        (1, 0): "4",
+        (1, 1): "5",
+        (1, 2): "6",
+        (2, 0): "1",
+        (2, 1): "2",
+        (2, 2): "3",
+        (3, 1): "0",
+        (3, 2): "A",
+    }
+    dir_pad = {
+        (0, 1): "^",
+        (0, 2): "A",
+        (1, 0): "<",
+        (1, 1): "v",
+        (1, 2): ">",
+    }
+
+    num_pad_paths = {
+        (num_pad[p1], num_pad[p2]): find_best_x_to_y(num_pad, p1, p2)
+        for p1 in num_pad
+        for p2 in num_pad
+    }
+    dir_pad_paths = {
+        (dir_pad[p1], dir_pad[p2]): find_best_x_to_y(dir_pad, p1, p2)
+        for p1 in dir_pad
+        for p2 in dir_pad
+    }
+
+    res = 0
+    for code in lines:
+        best_length = float("inf")
+        for path1 in keys_to_paths(num_pad_paths, code):
+            best_length = min(best_length, get_length(path1, 2))
+        res += int(code[:-1]) * best_length
+    return res
+
+def dayTwentyone2():
+
+    #read
+    with open("Day21/21_2.txt", 'r') as file:
+        lines = list(map(str.strip, file.readlines()))
+
+    def find_best_x_to_y(keypad, x, y):
+        if x == y:
+            return ["A"]
+        # row, column, path
+        queue = deque([(*x, "")])
+        best_length = 10
+        optimal_paths = []
+        while queue:
+            r, c, path = queue.popleft()
+            for nr, nc, move in [
+                (r + 1, c, "v"),
+                (r - 1, c, "^"),
+                (r, c + 1, ">"),
+                (r, c - 1, "<"),
+            ]:
+                if (nr, nc) not in keypad:
+                    continue
+                if (nr, nc) == y and len(path) + 1 > best_length:
+                    break
+                if (nr, nc) == y:
+                    optimal_paths.append(path + move + "A")
+                    best_length = min(len(path) + 2, best_length)
+                else:
+                    queue.append((nr, nc, path + move))
+            else:
+                continue
+            break
+        return optimal_paths
+
+    def keys_to_paths(keypad_paths, code):
+        paths = [keypad_paths[(b1, b2)] for b1, b2 in zip("A" + code, code)]
+        return ["".join(segment) for segment in product(*paths)]
+
+    @cache
+    def get_length(code, depth):
+        if depth == 0:
+            return len(code)
+        length = 0
+        for p1, p2 in zip("A" + code, code):
+            length += min(get_length(seq, depth - 1) for seq in dir_pad_paths[(p1, p2)])
+        return length
+
+    num_pad = {
+        (0, 0): "7",
+        (0, 1): "8",
+        (0, 2): "9",
+        (1, 0): "4",
+        (1, 1): "5",
+        (1, 2): "6",
+        (2, 0): "1",
+        (2, 1): "2",
+        (2, 2): "3",
+        (3, 1): "0",
+        (3, 2): "A",
+    }
+    dir_pad = {
+        (0, 1): "^",
+        (0, 2): "A",
+        (1, 0): "<",
+        (1, 1): "v",
+        (1, 2): ">",
+    }
+
+    num_pad_paths = {
+        (num_pad[p1], num_pad[p2]): find_best_x_to_y(num_pad, p1, p2)
+        for p1 in num_pad
+        for p2 in num_pad
+    }
+    dir_pad_paths = {
+        (dir_pad[p1], dir_pad[p2]): find_best_x_to_y(dir_pad, p1, p2)
+        for p1 in dir_pad
+        for p2 in dir_pad
+    }
+
+    res = 0
+    for code in lines:
+        best_length = float("inf")
+        for path1 in keys_to_paths(num_pad_paths, code):
+            best_length = min(best_length, get_length(path1, 25))
+        res += int(code[:-1]) * best_length
+    return res
+
+def main():
+    print("Hallo")
+    print(dayTwentyone(), "ist die Lösung von Teil 1")
+    print(dayTwentyone2(), "ist die Lösung von Teil 2")
+
+if __name__=="__main__":
+    main()
